7e58f9f450f5573af1f101de3fa82ebe824e5060
awirshj <128029644+awirshj@users.noreply.github.com>
2023-05-15 01:42:33 +0330

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
 	// GENERAL VARIABLES
+
+	const imgTargets = document.querySelectorAll("img[data-src]");
 	const allSections = Array.from(document.getElementsByTagName("section"));
 	const sectionHero = document.querySelector(".section__hero");
 	/*remove hero section from all sections */
@@ -11,11 +13,15 @@ document.addEventListener("DOMContentLoaded", function () {
 		".hero__text-container--inner .help--flex-row"
 	);
 	const howTabBtns = document.querySelector(".how__operations__tab--container");
+	const mobileMenuOpen = document.querySelector(".nav--icon-open");
+	console.log(mobileMenuOpen);
+	const mobileMenuClose = document.querySelector(".btn__mobile--close");
 	// labels
 	const logo = Array.from(document.querySelectorAll(".nav__logo"));
 	// section hero
 	const mainNavLinks = document.querySelector(".main-nav__list-links");
 	const header = document.querySelector(".header");
+	const headerNav = document.querySelector(".header--nav");
 	// section how
 	const howTabTextBox = Array.from(
 		document.querySelectorAll(".operations__content ")
@@ -83,92 +89,104 @@ document.addEventListener("DOMContentLoaded", function () {
 			sectionHero.scrollIntoView({ behavior: "smooth" });
 		});
 	});
+
 	// reveal sections
 	const revealSectionsObs = new IntersectionObserver(
 		(entries, observe) => {
 			const [entry] = entries;
 			if (!entry.isIntersecting) return;
-			console.log(entry.isIntersecting);
 			entry.target.classList.remove("section--hidden");
 			observe.unobserve(entry.target);
 		},
-		{ root: null, threshold: 0.17 }
+		{ root: null, threshold: [0.1, 0.2] }
 	);
 	allSections.forEach((sect) => {
 		sect.classList.add("section--hidden");
 		revealSectionsObs.observe(sect);
 	});
 	revealSectionsObs.observe(footer);
+
+	// mobile nav(hamburger menu)
+	mobileMenuOpen.addEventListener("click", function () {
+		headerNav.classList.remove("mobile--nav-hidden");
+	});
+	mobileMenuClose.addEventListener("click", function () {
+		headerNav.classList.add("mobile--nav-hidden");
+	});
+	// lazy loading img
+	const imgLoadFn = (entries, observer) => {
+		const [entry] = entries;
+		console.log(entry);
+		if (!entry.isIntersecting) return;
+		entry.target.src = entry.target.dataset.src;
+		entry.target.addEventListener("load", function () {
+			entry.target.classList.remove("lazy-img");
+		});
+		observer.unobserve(entry.target);
+	};
+	const imgLoadObs = new IntersectionObserver(
+		imgLoadFn,
+		// [0, 0.1, 0.25, 0.5, 1] because of revealing sections
+		{ root: null, threshold: [0, 0.1, 0.25, 0.5, 1] }
+	);
+	imgTargets.forEach((img) => imgLoadObs.observe(img));
 	// section how: operator(slider)
 	howTabBtns.addEventListener("click", function (e) {
 		if (!e.target.closest(".operations__tab")) return;
 		// stepNum is zero based
 		const stepNum = Number(e.target.closest(".btn").dataset.step);
-		// style tap
+		// style tap btn
 		howTabBtns.querySelectorAll(".operations__tab").forEach((tab, i) => {
 			i === stepNum
 				? tab.classList.add("operations__tab--active")
 				: tab.classList.remove("operations__tab--active");
 		});
-		//  show active tab
+		//  show active tab(content)
 		howTabTextBox.forEach((tab, i) =>
 			i === stepNum
 				? tab.classList.add("operations__content--active")
 				: tab.classList.remove("operations__content--active")
 		);
 	});
+
 	// section testimonial: operator(slider)
-	testCardSwapperRight.addEventListener("click", function (e) {
+	const testSliderFn = function () {
 		let activeIndex;
-		let slideNum;
+		let nextSlide;
+		// type = right or left.
+		this === "Right" ? (nextSlide = 2) : (nextSlide = 4);
 		// test__card-slide--1
-		testCardSlides.forEach((slide, i, arr) => {
+		testCardSlides.forEach((slide, i) => {
 			slide.classList.contains("test__card--active") ? (activeIndex = i) : "";
-			slide.classList.contains("test__card--active")
-				? (slideNum = slide.dataset.slide)
-				: "";
-			console.log(slide);
 		});
-		testCardSlides.forEach((slide, i, arr) => {
+		testCardSlides.forEach((slide, i) => {
+			if (slide.classList.contains(`test__card-slide--${nextSlide}`)) {
+				slide.classList.remove(`test__card-slide--${nextSlide}`);
+				// active next slide
+				slide.classList.add(`test__card-slide--1`);
+				slide.classList.add("test__card--active");
+			} else if (slide.classList.contains(`test__card-slide--3`)) {
+				slide.classList.remove(`test__card-slide--3`);
+				slide.classList.add(
+					`test__card-slide--${3 + (nextSlide === 2 ? -1 : 1)}`
+				);
+			} else if (
+				slide.classList.contains(`test__card-slide--${nextSlide === 2 ? 4 : 2}`)
+			) {
+				slide.classList.remove(`test__card-slide--${nextSlide === 2 ? 4 : 2}`);
+				slide.classList.add(`test__card-slide--3`);
+			}
 			if (activeIndex === i) {
 				slide.classList.remove("test__card-slide--1");
 				slide.classList.remove("test__card--active");
-				slide.classList.add("test__card-slide--4");
-			} else if (slide.classList.contains("test__card-slide--4")) {
-				slide.classList.remove("test__card-slide--4");
-				slide.classList.add("test__card-slide--3");
-			} else if (slide.classList.contains("test__card-slide--3")) {
-				slide.classList.remove("test__card-slide--3");
-				slide.classList.add("test__card-slide--2");
-			} else if (slide.classList.contains("test__card-slide--2")) {
-				slide.classList.remove("test__card-slide--2");
-				slide.classList.add("test__card-slide--1");
-				slide.classList.add("test__card--active");
+				[2, 4].forEach((num) =>
+					num === nextSlide
+						? ""
+						: slide.classList.add(`test__card-slide--${num}`)
+				);
 			}
 		});
-	});
-	testCardSwapperLeft.addEventListener("click", function (e) {
-		let activeIndex;
-		// test__card-slide--1
-		testCardSlides.forEach((slide, i, arr) => {
-			slide.classList.contains("test__card--active") ? (activeIndex = i) : "";
-		});
-		testCardSlides.forEach((slide, i, arr) => {
-			if (activeIndex === i) {
-				slide.classList.remove("test__card-slide--1");
-				slide.classList.remove("test__card--active");
-				slide.classList.add("test__card-slide--2");
-			} else if (slide.classList.contains("test__card-slide--4")) {
-				slide.classList.remove("test__card-slide--4");
-				slide.classList.add("test__card-slide--1");
-				slide.classList.add("test__card--active");
-			} else if (slide.classList.contains("test__card-slide--3")) {
-				slide.classList.remove("test__card-slide--3");
-				slide.classList.add("test__card-slide--4");
-			} else if (slide.classList.contains("test__card-slide--2")) {
-				slide.classList.remove("test__card-slide--2");
-				slide.classList.add("test__card-slide--3");
-			}
-		});
-	});
+	};
+	testCardSwapperRight.addEventListener("click", testSliderFn.bind("Right"));
+	testCardSwapperLeft.addEventListener("click", testSliderFn.bind("left"));
 });
